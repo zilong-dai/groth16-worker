4a75bc4430bb50b6511f44c810a7cdda728e5bba
zilong.dai <2840478265@qq.com>
2024-05-21 18:54:40 +0800

    feat: change proof, vk Marshal to compress form

--- worker/rpc/service.go
+++ worker/rpc/service.go
@@ -1,12 +1,14 @@
 package rpc
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
+	"time"
 )
 
 func (ws *WorkerService) Run(port int) {
@@ -29,20 +31,6 @@ func (ws *WorkerService) Run(port int) {
 
 }
 
-func (ws *WorkerService) Build(args []string, reply *string) error {
-	if len(args) != 3 {
-		return errors.New("arg is not 3")
-	}
-
-	if ws.worker == nil {
-		return errors.New("worker is nil")
-	}
-
-	*reply = ws.worker.Build(args[0], args[1], args[2])
-
-	return nil
-}
-
 func (ws *WorkerService) GenerateProof(args []string, reply *string) error {
 	if len(args) != 3 {
 		return errors.New("arg is not 3")
@@ -52,12 +40,20 @@ func (ws *WorkerService) GenerateProof(args []string, reply *string) error {
 		return errors.New("worker is nil")
 	}
 
+	if !ws.worker.IsSetup {
+		*reply = ws.worker.Build(args[0], args[1], args[2])
+		if *reply != "true" {
+			return errors.New("build failed")
+		}
+		ws.worker.IsSetup = true
+	}
+
 	*reply = ws.worker.GenerateProof(args[0], args[1], args[2])
 	return nil
 }
 
 func (ws *WorkerService) VerifyProof(args *string, reply *string) error {
-	fmt.Println(args)
+	fmt.Println("call verifyProof", time.Now())
 	if args == nil {
 		return errors.New("args is nil")
 	}
@@ -70,5 +66,25 @@ func (ws *WorkerService) VerifyProof(args *string, reply *string) error {
 
 	*reply = ws.worker.VerifyProof(*args)
 
+	return nil
+}
+
+func (ws *WorkerService) GetVK(args *string, reply *string) error {
+	if args == nil {
+		return errors.New("args is nil")
+	}
+	if *args == "" {
+		return errors.New("proofString is empty")
+	}
+	if ws.worker == nil || ws.worker.vk == nil {
+		return errors.New("worker is nil")
+	}
+
+	vkBytes, err := json.Marshal(ws.worker.vk)
+	if err != nil {
+		*reply = "false"
+		return fmt.Errorf("failed to marshal vk: %v", err)
+	}
+	*reply = string(vkBytes)
 	return nil
 }
--- worker/rpc/worker.go
+++ worker/rpc/worker.go
@@ -2,12 +2,15 @@ package rpc
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"math/big"
 
 	curve "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	fp "github.com/consensys/gnark-crypto/ecc/bls12-381/fp"
+	fr_bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 	"github.com/zilong-dai/gnark-plonky2-verifier/plonk/gates"
 	"github.com/zilong-dai/gnark-plonky2-verifier/types"
 	"github.com/zilong-dai/gnark-plonky2-verifier/variables"
@@ -200,10 +203,13 @@ func (w *Groth16Prover) VerifyProof(proofString string) string {
 		return "false"
 	}
 
+	fmt.Println("start verify")
+
 	if err := groth16.Verify(g16ProofWithPublicInputs.Proof, w.vk, g16ProofWithPublicInputs.PublicInputs); err != nil {
 		fmt.Println("verify proof failed %v", err)
 		return "false"
 	}
+	fmt.Println("true")
 	return "true"
 }
 
@@ -222,6 +228,142 @@ func Chunk(str string, chunk int) []string {
 	return res
 }
 
+func hexToBigInt(hex string) (*big.Int, error) {
+	n, success := big.NewInt(0).SetString(hex, 16)
+	if success {
+		return n, nil
+	} else {
+		return nil, fmt.Errorf("invalid hex string: %s", hex)
+	}
+}
+
+func isOddFp(x *fp.Element) bool {
+	return x.BigInt(big.NewInt(0)).Bit(0) == 1
+}
+
+func reverseHexString(hexStr string) string {
+	reversed := make([]byte, len(hexStr))
+	for i := 0; i < len(hexStr); i += 2 {
+		reversed[i] = hexStr[len(hexStr)-i-2]
+		reversed[i+1] = hexStr[len(hexStr)-i-1]
+	}
+	return string(reversed)
+
+}
+func serializeG1(g1 *curve.G1Affine) (string, error) {
+	// cc, _ := new(big.Int).SetString(g1.Y.Text(16), 16)
+	// var elementY fp.Element
+	// elementY.SetString(cc.String())
+	// if isOddFp(&g1.Y) {
+	// 	xBytes, err := hex.DecodeString(g1.X.Text(16))
+	// 	if err != nil {
+	// 		return "", err
+	// 	}
+	// 	xBytes[0] |= 0x80
+	// 	return reverseHexString(hex.EncodeToString(xBytes)), nil
+	// } else {
+	// 	return reverseHexString(g1.X.Text(16)), nil
+	// }
+
+	g1Bytes := g1.Bytes()
+	return reverseHexString(hex.EncodeToString(g1Bytes[:])), nil
+
+}
+
+func serializeG2(g2 *curve.G2Affine) (string, error) {
+	// cc, _ := new(big.Int).SetString(g2.Y.A0, 16)
+	// var elementY fp.Element
+	// elementY.SetString(cc.String())
+
+	// if isOddFp(&g2.Y.A0) {
+	// 	xBytes, err := hex.DecodeString(g2.X.A1.Text(16))
+	// 	if err != nil {
+	// 		return "", err
+	// 	}
+	// 	xBytes[0] |= 0x80
+	// 	return reverseHexString(g2.X.A0.Text(16)) + reverseHexString(hex.EncodeToString(xBytes)), nil
+	// } else {
+	// 	return reverseHexString(g2.X.A0.Text(16)) + reverseHexString(g2.X.A1.Text(16)), nil
+	// }
+	g2Bytes := g2.Bytes()
+	return reverseHexString(hex.EncodeToString(g2Bytes[48:])) + reverseHexString(hex.EncodeToString(g2Bytes[:48])), nil
+
+}
+
+func deserializeG1(g string) (*curve.G1Affine, error) {
+	if len(g) != 96 {
+		return nil, fmt.Errorf("invalid G1 point length %d", len(g))
+	}
+
+	// xBytes, err := hex.DecodeString(reverseHexString(g))
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// // mask := xBytes[0] & 0x80
+	// fmt.Println("mask", reverseHexString(g))
+
+	// // if mask == 0x80 {
+	// // 	xBytes[0] |= 0xa0
+	// // } else {
+	// // 	xBytes[0] |= 0x80
+	// // }
+	// xBytes[0] |= 0xa0
+
+	// g1 := new(curve.G1Affine)
+	// if _, err := g1.SetBytes(xBytes); err != nil {
+	// 	fmt.Println(err)
+	// 	return nil, err
+	// }
+
+	g1 := new(curve.G1Affine)
+	g1Bytes, err := hex.DecodeString(reverseHexString(g))
+	if err != nil {
+		return nil, fmt.Errorf("hex decode error")
+	}
+
+	if _, err := g1.SetBytes(g1Bytes); err != nil {
+		return nil, err
+	}
+
+	return g1, nil
+}
+
+func deserializeG2(g string) (*curve.G2Affine, error) {
+	if len(g) != 192 {
+		return nil, fmt.Errorf("invalid G2 point length %d", len(g))
+	}
+
+	// xBytes, err := hex.DecodeString(reverseHexString(g))
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// mask := xBytes[0] & 0x80
+
+	// if mask == 0x80 {
+	// 	xBytes[0] &= 0xa0
+	// } else {
+	// 	xBytes[0] &= 0x80
+	// }
+
+	// g2 := new(curve.G2Affine)
+	// if _, err := g2.SetBytes(xBytes); err != nil {
+	// 	return nil, err
+	// }
+	g2 := new(curve.G2Affine)
+	g2Bytes, err := hex.DecodeString(reverseHexString(g))
+	if err != nil {
+		return nil, fmt.Errorf("hex decode error")
+	}
+
+	if _, err := g2.SetBytes(g2Bytes); err != nil {
+		return nil, err
+	}
+
+	return g2, nil
+}
+
 func (p G16ProofWithPublicInputs) MarshalJSON() ([]byte, error) {
 
 	proof := p.Proof.(*bls12381.Proof)
@@ -237,24 +379,47 @@ func (p G16ProofWithPublicInputs) MarshalJSON() ([]byte, error) {
 		}
 	}
 
-	pi_a_arr := Chunk(hex.EncodeToString((&proof.Ar).Marshal()), 2)
-	pi_b_arr := Chunk(hex.EncodeToString((&proof.Bs).Marshal()), 4)
-	pi_c_arr := Chunk(hex.EncodeToString((&proof.Krs).Marshal()), 2)
-
-	var buffer bytes.Buffer
-	_, err := p.PublicInputs.WriteTo(&buffer)
+	// pi_a_arr := Chunk(hex.EncodeToString((&proof.Ar).Marshal()), 2)
+	// pi_b_arr := Chunk(hex.EncodeToString((&proof.Bs).Marshal()), 4)
+	// pi_c_arr := Chunk(hex.EncodeToString((&proof.Krs).Marshal()), 2)
+	piASerialized, err := serializeG1(&proof.Ar)
 	if err != nil {
 		return nil, err
 	}
-	public_inputs_arr := hex.EncodeToString(buffer.Bytes())[24:]
+	piCSerialized, err := serializeG1(&proof.Krs)
+	if err != nil {
+		return nil, err
+	}
 
+	piBSerialized, err := serializeG2(&proof.Bs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	vectors := p.PublicInputs.Vector().(fr_bls12381.Vector)
+	public_inputs_arr := make([]string, len(vectors))
+	for i, vec := range vectors {
+		vecBytes := vec.Bytes()
+		public_inputs_arr[i] = reverseHexString(hex.EncodeToString(vecBytes[:]))
+	}
+
+	// proof_map := map[string]interface{}{
+	// 	"pi_a":          [2]string{pi_a_arr[0], pi_a_arr[1]},
+	// 	"pi_b":          [2][2]string{{pi_b_arr[1], pi_b_arr[0]}, {pi_b_arr[3], pi_b_arr[2]}},
+	// 	"pi_c":          [2]string{pi_c_arr[0], pi_c_arr[1]},
+	// 	"Commitments":   hex.EncodeToString(writer.Bytes()),
+	// 	"CommitmentPok": hex.EncodeToString((&proof.CommitmentPok).Marshal()),
+	// 	"public_inputs": [2]string{public_inputs_arr[0:64], public_inputs_arr[64:128]},
+	// }
 	proof_map := map[string]interface{}{
-		"pi_a":          [2]string{pi_a_arr[0], pi_a_arr[1]},
-		"pi_b":          [2][2]string{{pi_b_arr[1], pi_b_arr[0]}, {pi_b_arr[3], pi_b_arr[2]}},
-		"pi_c":          [2]string{pi_c_arr[0], pi_c_arr[1]},
+		"pi_a":          piASerialized,
+		"pi_b_a0":       piBSerialized[:96],
+		"pi_b_a1":       piBSerialized[96:],
+		"pi_c":          piCSerialized,
 		"Commitments":   hex.EncodeToString(writer.Bytes()),
 		"CommitmentPok": hex.EncodeToString((&proof.CommitmentPok).Marshal()),
-		"public_inputs": [2]string{public_inputs_arr[0:64], public_inputs_arr[64:128]},
+		"public_inputs": public_inputs_arr,
 	}
 	return json.Marshal(proof_map)
 
@@ -263,12 +428,13 @@ func (p G16ProofWithPublicInputs) MarshalJSON() ([]byte, error) {
 func (p *G16ProofWithPublicInputs) UnmarshalJSON(data []byte) error {
 	proof := p.Proof.(*bls12381.Proof)
 	var ProofString struct {
-		PiA           [2]string    `json:"pi_a"`
-		PiB           [2][2]string `json:"pi_b"`
-		PiC           [2]string    `json:"pi_c"`
-		Commitments   string       `json:"Commitments"`
-		CommitmentPok string       `json:"CommitmentPok"`
-		PublicInputs  [2]string    `json:"public_inputs"`
+		PiA           string   `json:"pi_a"`
+		PiB_A0        string   `json:"pi_b_a0"`
+		PiB_A1        string   `json:"pi_b_a1"`
+		PiC           string   `json:"pi_c"`
+		Commitments   string   `json:"Commitments"`
+		CommitmentPok string   `json:"CommitmentPok"`
+		PublicInputs  []string `json:"public_inputs"`
 	}
 
 	err := json.Unmarshal(data, &ProofString)
@@ -276,40 +442,31 @@ func (p *G16ProofWithPublicInputs) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	pia_bytes, err := hex.DecodeString(ProofString.PiA[0] + ProofString.PiA[1])
+	ar, err := deserializeG1(ProofString.PiA)
 	if err != nil {
 		return err
 	}
-	err = proof.Ar.Unmarshal(pia_bytes)
-	if err != nil {
-		return err
-	}
+	proof.Ar = *ar
 
-	pib_bytes, err := hex.DecodeString(ProofString.PiB[0][1] + ProofString.PiB[0][0] + ProofString.PiB[1][1] + ProofString.PiB[1][0])
+	krs, err := deserializeG1(ProofString.PiC)
 	if err != nil {
 		return err
 	}
-	err = proof.Bs.Unmarshal(pib_bytes)
-	if err != nil {
-		return err
-	}
+	proof.Krs = *krs
 
-	pic_bytes, err := hex.DecodeString(ProofString.PiC[0] + ProofString.PiC[1])
+	bs, err := deserializeG2(ProofString.PiB_A0 + ProofString.PiB_A1)
 	if err != nil {
 		return err
 	}
-	err = proof.Krs.Unmarshal(pic_bytes)
-	if err != nil {
-		return err
-	}
+	proof.Bs = *bs
 
 	com_bytes, err := hex.DecodeString(ProofString.Commitments)
 	if err != nil {
 		return err
 	}
-	len := len(com_bytes) / 96
-	proof.Commitments = make([]curve.G1Affine, len)
-	for i := 0; i < len; i++ {
+	com_len := len(com_bytes) / 96
+	proof.Commitments = make([]curve.G1Affine, com_len)
+	for i := 0; i < com_len; i++ {
 		err = proof.Commitments[i].Unmarshal(com_bytes[96*i : 96*(i+1)])
 		if err != nil {
 			return err
@@ -326,17 +483,33 @@ func (p *G16ProofWithPublicInputs) UnmarshalJSON(data []byte) error {
 	}
 
 	// public inputs num 2, witness inputs num 0, vector length 2
-	publicinputs_bytes, err := hex.DecodeString("000000020000000000000002" + ProofString.PublicInputs[0] + ProofString.PublicInputs[1])
 
-	if err != nil {
+	// vectors := make(fr_bls12381.Vector, len(ProofString.PublicInputs))
+
+	var wr bytes.Buffer
+	if err := binary.Write(&wr, binary.BigEndian, uint32(len(ProofString.PublicInputs))); err != nil {
 		return err
 	}
 
-	reader := bytes.NewReader(publicinputs_bytes)
-
-	if _, err = p.PublicInputs.ReadFrom(reader); err != nil {
+	if err := binary.Write(&wr, binary.BigEndian, uint32(0)); err != nil {
 		return err
 	}
+
+	if err := binary.Write(&wr, binary.BigEndian, uint32(len(ProofString.PublicInputs))); err != nil {
+		return err
+	}
+	for _, pub := range ProofString.PublicInputs {
+		vecBytes, err := hex.DecodeString(reverseHexString(pub))
+		if err != nil {
+			return err
+		}
+
+		if _, err := wr.Write(vecBytes); err != nil {
+			return err
+		}
+	}
+	rr := bytes.NewReader(wr.Bytes())
+	p.PublicInputs.ReadFrom(rr)
 
 	return nil
 }
@@ -371,35 +544,173 @@ func (p *G16ProofWithPublicInputs) UnmarshalJSON(data []byte) error {
 //     pub gamma_abc_g1: Vec<E::G1Affine>,
 // }
 
+// func (gvk G16VerifyingKey) MarshalJSON() ([]byte, error) {
+// 	vk := gvk.VK.(*bls12381.VerifyingKey)
+// 	var buf [48 * 2]byte
+
+// 	gamma_abc_g1_arr := make([][]string, len(vk.G1.K))
+// 	for i := 0; i < len(vk.G1.K); i++ {
+// 		gamma_abc_g1_arr[i] = make([]string, 2)
+// 	}
+// 	for i := 0; i < len(vk.G1.K); i++ {
+// 		buf = vk.G1.K[i].RawBytes()
+// 		gamma_abc_g1_arr[i][0] = hex.EncodeToString(buf[:])[0:96]
+// 		gamma_abc_g1_arr[i][1] = hex.EncodeToString(buf[:])[96:192]
+// 	}
+
+// 	var comkey_writer bytes.Buffer
+
+// 	vk.CommitmentKey.WriteRawTo(&comkey_writer)
+
+// 	alpha_g1_arr := Chunk(hex.EncodeToString((&vk.G1.Alpha).Marshal()), 2)
+// 	beta_g2_arr := Chunk(hex.EncodeToString((&vk.G2.Beta).Marshal()), 4)
+// 	gamma_g2_arr := Chunk(hex.EncodeToString((&vk.G2.Gamma).Marshal()), 4)
+// 	delta_g2_arr := Chunk(hex.EncodeToString((&vk.G2.Delta).Marshal()), 4)
+// 	CommitmentKey := hex.EncodeToString(comkey_writer.Bytes())
+
+// 	vk_map := map[string]interface{}{
+// 		"alpha_g1":      [2]string{alpha_g1_arr[0], alpha_g1_arr[1]},
+// 		"beta_g2":       [2][2]string{{beta_g2_arr[1], beta_g2_arr[0]}, {beta_g2_arr[3], beta_g2_arr[2]}},
+// 		"gamma_g2":      [2][2]string{{gamma_g2_arr[1], gamma_g2_arr[0]}, {gamma_g2_arr[3], gamma_g2_arr[2]}},
+// 		"delta_g2":      [2][2]string{{delta_g2_arr[1], delta_g2_arr[0]}, {delta_g2_arr[3], delta_g2_arr[2]}},
+// 		"gamma_abc_g1":  gamma_abc_g1_arr,
+// 		"CommitmentKey": CommitmentKey,
+// 		// "CommitmentKeyG": hex.EncodeToString((&vk.CommitmentKey.g).Marshal()),
+// 		// "CommitmentKeyGRoot": hex.EncodeToString((&vk.CommitmentKey.gRootSigmaNeg).Marshal()),
+// 		"PublicAndCommitmentCommitted": vk.PublicAndCommitmentCommitted,
+// 	}
+
+// 	return json.Marshal(vk_map)
+// }
+
+// func (gvk *G16VerifyingKey) UnmarshalJSON(data []byte) error {
+// 	vk := gvk.VK.(*bls12381.VerifyingKey)
+// 	var VerifyingKeyString struct {
+// 		Alpha         [2]string    `json:"alpha_g1"`
+// 		K             [][]string   `json:"gamma_abc_g1"`
+// 		Beta          [2][2]string `json:"beta_g2"`
+// 		Gamma         [2][2]string `json:"gamma_g2"`
+// 		Delta         [2][2]string `json:"delta_g2"`
+// 		CommitmentKey string       `json:"CommitmentKey"`
+// 		// CommitmentKeyG string
+// 		// CommitmentKeyGRoot string
+// 		PublicAndCommitmentCommitted [][]int `json:"PublicAndCommitmentCommitted"`
+// 	}
+
+// 	err := json.Unmarshal(data, &VerifyingKeyString)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	alpha_bytes, err := hex.DecodeString(VerifyingKeyString.Alpha[0] + VerifyingKeyString.Alpha[1])
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = vk.G1.Alpha.Unmarshal(alpha_bytes)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	len := len(VerifyingKeyString.K)
+// 	vk.G1.K = make([]curve.G1Affine, len)
+// 	for i := 0; i < len; i++ {
+// 		k_bytes, err := hex.DecodeString(VerifyingKeyString.K[i][0] + VerifyingKeyString.K[i][1])
+// 		if err != nil {
+// 			return err
+// 		}
+// 		err = vk.G1.K[i].Unmarshal(k_bytes)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	beta_bytes, err := hex.DecodeString(VerifyingKeyString.Beta[0][1] + VerifyingKeyString.Beta[0][0] + VerifyingKeyString.Beta[1][1] + VerifyingKeyString.Beta[1][0])
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = vk.G2.Beta.Unmarshal(beta_bytes)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	gamma_bytes, err := hex.DecodeString(VerifyingKeyString.Gamma[0][1] + VerifyingKeyString.Gamma[0][0] + VerifyingKeyString.Gamma[1][1] + VerifyingKeyString.Gamma[1][0])
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = vk.G2.Gamma.Unmarshal(gamma_bytes)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	delta_bytes, err := hex.DecodeString(VerifyingKeyString.Delta[0][1] + VerifyingKeyString.Delta[0][0] + VerifyingKeyString.Delta[1][1] + VerifyingKeyString.Delta[1][0])
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = vk.G2.Delta.Unmarshal(delta_bytes)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	comkey_bytes, err := hex.DecodeString(VerifyingKeyString.CommitmentKey)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	_, err = vk.CommitmentKey.ReadFrom(bytes.NewReader(comkey_bytes))
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	vk.PublicAndCommitmentCommitted = VerifyingKeyString.PublicAndCommitmentCommitted
+
+// 	err = vk.Precompute()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
 func (gvk G16VerifyingKey) MarshalJSON() ([]byte, error) {
 	vk := gvk.VK.(*bls12381.VerifyingKey)
-	var buf [48 * 2]byte
+	// var buf [48 * 2]byte
 
-	gamma_abc_g1_arr := make([][]string, len(vk.G1.K))
-	for i := 0; i < len(vk.G1.K); i++ {
-		gamma_abc_g1_arr[i] = make([]string, 2)
-	}
-	for i := 0; i < len(vk.G1.K); i++ {
-		buf = vk.G1.K[i].RawBytes()
-		gamma_abc_g1_arr[i][0] = hex.EncodeToString(buf[:])[0:96]
-		gamma_abc_g1_arr[i][1] = hex.EncodeToString(buf[:])[96:192]
+	gamma_abc_g1_arr := make([]string, len(vk.G1.K))
+	for i, k := range vk.G1.K {
+		var err error
+		gamma_abc_g1_arr[i], err = serializeG1(&k)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	var comkey_writer bytes.Buffer
 
 	vk.CommitmentKey.WriteRawTo(&comkey_writer)
 
-	alpha_g1_arr := Chunk(hex.EncodeToString((&vk.G1.Alpha).Marshal()), 2)
-	beta_g2_arr := Chunk(hex.EncodeToString((&vk.G2.Beta).Marshal()), 4)
-	gamma_g2_arr := Chunk(hex.EncodeToString((&vk.G2.Gamma).Marshal()), 4)
-	delta_g2_arr := Chunk(hex.EncodeToString((&vk.G2.Delta).Marshal()), 4)
+	alpha_g1_arr, err := serializeG1(&vk.G1.Alpha)
+	if err != nil {
+		return nil, err
+	}
+	beta_g2_arr, err := serializeG2(&vk.G2.Beta)
+	if err != nil {
+		return nil, err
+	}
+	gamma_g2_arr, err := serializeG2(&vk.G2.Gamma)
+	if err != nil {
+		return nil, err
+	}
+	delta_g2_arr, err := serializeG2(&vk.G2.Delta)
+	if err != nil {
+		return nil, err
+	}
+
 	CommitmentKey := hex.EncodeToString(comkey_writer.Bytes())
 
 	vk_map := map[string]interface{}{
-		"alpha_g1":      [2]string{alpha_g1_arr[0], alpha_g1_arr[1]},
-		"beta_g2":       [2][2]string{{beta_g2_arr[1], beta_g2_arr[0]}, {beta_g2_arr[3], beta_g2_arr[2]}},
-		"gamma_g2":      [2][2]string{{gamma_g2_arr[1], gamma_g2_arr[0]}, {gamma_g2_arr[3], gamma_g2_arr[2]}},
-		"delta_g2":      [2][2]string{{delta_g2_arr[1], delta_g2_arr[0]}, {delta_g2_arr[3], delta_g2_arr[2]}},
+		"alpha_g1":      alpha_g1_arr,
+		"beta_g2":       beta_g2_arr,
+		"gamma_g2":      gamma_g2_arr,
+		"delta_g2":      delta_g2_arr,
 		"gamma_abc_g1":  gamma_abc_g1_arr,
 		"CommitmentKey": CommitmentKey,
 		// "CommitmentKeyG": hex.EncodeToString((&vk.CommitmentKey.g).Marshal()),
@@ -413,12 +724,12 @@ func (gvk G16VerifyingKey) MarshalJSON() ([]byte, error) {
 func (gvk *G16VerifyingKey) UnmarshalJSON(data []byte) error {
 	vk := gvk.VK.(*bls12381.VerifyingKey)
 	var VerifyingKeyString struct {
-		Alpha         [2]string    `json:"alpha_g1"`
-		K             [][]string   `json:"gamma_abc_g1"`
-		Beta          [2][2]string `json:"beta_g2"`
-		Gamma         [2][2]string `json:"gamma_g2"`
-		Delta         [2][2]string `json:"delta_g2"`
-		CommitmentKey string       `json:"CommitmentKey"`
+		Alpha         string   `json:"alpha_g1"`
+		K             []string `json:"gamma_abc_g1"`
+		Beta          string   `json:"beta_g2"`
+		Gamma         string   `json:"gamma_g2"`
+		Delta         string   `json:"delta_g2"`
+		CommitmentKey string   `json:"CommitmentKey"`
 		// CommitmentKeyG string
 		// CommitmentKeyGRoot string
 		PublicAndCommitmentCommitted [][]int `json:"PublicAndCommitmentCommitted"`
@@ -429,54 +740,39 @@ func (gvk *G16VerifyingKey) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	alpha_bytes, err := hex.DecodeString(VerifyingKeyString.Alpha[0] + VerifyingKeyString.Alpha[1])
+	alpha, err := deserializeG1(VerifyingKeyString.Alpha)
 	if err != nil {
 		return err
 	}
-	err = vk.G1.Alpha.Unmarshal(alpha_bytes)
-	if err != nil {
-		return err
-	}
+	vk.G1.Alpha = *alpha
 
 	len := len(VerifyingKeyString.K)
 	vk.G1.K = make([]curve.G1Affine, len)
 	for i := 0; i < len; i++ {
-		k_bytes, err := hex.DecodeString(VerifyingKeyString.K[i][0] + VerifyingKeyString.K[i][1])
-		if err != nil {
-			return err
+		k, err := deserializeG1(VerifyingKeyString.K[i])
+		if nil != err {
+			return nil
 		}
-		err = vk.G1.K[i].Unmarshal(k_bytes)
-		if err != nil {
-			return err
-		}
+		vk.G1.K[i] = *k
 	}
 
-	beta_bytes, err := hex.DecodeString(VerifyingKeyString.Beta[0][1] + VerifyingKeyString.Beta[0][0] + VerifyingKeyString.Beta[1][1] + VerifyingKeyString.Beta[1][0])
-	if err != nil {
-		return err
+	beta, err := deserializeG2(VerifyingKeyString.Beta)
+	if nil != err {
+		return nil
 	}
-	err = vk.G2.Beta.Unmarshal(beta_bytes)
-	if err != nil {
-		return err
-	}
+	vk.G2.Beta = *beta
 
-	gamma_bytes, err := hex.DecodeString(VerifyingKeyString.Gamma[0][1] + VerifyingKeyString.Gamma[0][0] + VerifyingKeyString.Gamma[1][1] + VerifyingKeyString.Gamma[1][0])
-	if err != nil {
-		return err
+	gamma, err := deserializeG2(VerifyingKeyString.Gamma)
+	if nil != err {
+		return nil
 	}
-	err = vk.G2.Gamma.Unmarshal(gamma_bytes)
-	if err != nil {
-		return err
-	}
+	vk.G2.Gamma = *gamma
 
-	delta_bytes, err := hex.DecodeString(VerifyingKeyString.Delta[0][1] + VerifyingKeyString.Delta[0][0] + VerifyingKeyString.Delta[1][1] + VerifyingKeyString.Delta[1][0])
-	if err != nil {
-		return err
+	delta, err := deserializeG2(VerifyingKeyString.Delta)
+	if nil != err {
+		return nil
 	}
-	err = vk.G2.Delta.Unmarshal(delta_bytes)
-	if err != nil {
-		return err
-	}
+	vk.G2.Gamma = *delta
 
 	comkey_bytes, err := hex.DecodeString(VerifyingKeyString.CommitmentKey)
 	if err != nil {
